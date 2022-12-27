9315db009ff46eea579e2492cb950030e5d5c94d
test <anistiks@gmail.com>
2022-12-27 14:42:59 +0300

    Добавлен расчет бонусов

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 500;
+        int payment = 1000;
+
+        int bonus;
+
+        if (payment >= 1000) {
+            bonus = payment/100;
+        }
+        else {
+            bonus = 0;
+        }
+        int balance = amount + payment + bonus;
+        System.out.println("Баланс пополнен!" + "Сумма пополнения:" +payment + "рублей." + "Бонусов начислено:" + bonus);
+        System.out.println("Ваш баланс:" + balance);
+    }
+}
