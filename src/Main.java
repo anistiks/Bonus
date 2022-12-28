d23fd6e8926f60aca4f52b361bbb3eab2ebb4156
test <anistiks@gmail.com>
2022-12-28 15:04:17 +0300

    reformat and change payment

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         int amount = 500;
-        int payment = 1000;
+        int payment = 1100;
 
         int bonus;
 
-        if (payment >= 1000) {
-            bonus = payment/100;
-        }
-        else {
+        if (payment >= 1100) {
+            bonus = payment / 100;
+        } else {
             bonus = 0;
         }
         int balance = amount + payment + bonus;
-        System.out.println("Баланс пополнен!" + "Сумма пополнения:" +payment + "рублей." + "Бонусов начислено:" + bonus);
+        System.out.println("Баланс пополнен!" + "Сумма пополнения:" + payment + "рублей." + "Бонусов начислено:" + bonus);
         System.out.println("Ваш баланс:" + balance);
     }
 }
